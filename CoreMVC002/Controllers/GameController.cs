2c697283d0fa7d2fa9b7d8b98a16967ef117e48a
jessica930808 <a0905591780@gmail.com>
2024-10-16 21:00:24 +0800

    v0

---
+++ CoreMVC002/Controllers/GameController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CoreMVC002.Models;
+
+namespace CoreMVC002.Controllers
+{
+    public class GameController : Controller
+    {
+        private static XAXBEngine _engine = new XAXBEngine();
+
+        public IActionResult Index()
+        {
+            return View(_engine);
+        }
+
+        [HttpPost]
+        public IActionResult Guess(string guessNumber)
+        {
+            if (!_engine.IsGameOver(guessNumber))
+            {
+                ViewBag.Message = _engine.Result;
+            }
+            else
+            {
+                ViewBag.Message = "恭喜你猜對了！";
+                ViewBag.PromptReplay = true;
+            }
+
+            return View("Index", _engine);
+        }
+
+        [HttpPost]
+        public IActionResult Replay()
+        {
+            _engine.ResetGame();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ CoreMVC002/Models/XAXBEngine.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CoreMVC002.Models
+{
+    public class XAXBEngine
+    {
+        public string Secret { get; set; }
+        public string Guess { get; set; }
+        public string Result { get; set; }
+        public List<string> GuessHistory { get; set; } // 紀錄每次猜測的歷史
+        public int GuessCount { get; private set; } // 累計猜測次數
+
+        public XAXBEngine()
+        {
+            Secret = GenerateRandomSecret();
+            GuessHistory = new List<string>();
+            GuessCount = 0;
+            Guess = null;
+            Result = null;
+        }
+
+        public XAXBEngine(string secretNumber)
+        {
+            Secret = secretNumber;
+            GuessHistory = new List<string>();
+            GuessCount = 0;
+            Guess = null;
+            Result = null;
+        }
+
+        private string GenerateRandomSecret()
+        {
+            Random random = new Random();
+            return random.Next(1000, 9999).ToString(); // 隨機產生四位數的秘密數字
+        }
+
+        public int numOfA(string guessNumber)
+        {
+            int aCount = 0;
+            for (int i = 0; i < Secret.Length; i++)
+            {
+                if (Secret[i] == guessNumber[i])
+                {
+                    aCount++;
+                }
+            }
+            return aCount;
+        }
+
+        public int numOfB(string guessNumber)
+        {
+            int bCount = 0;
+            bool[] matched = new bool[Secret.Length];
+
+            for (int i = 0; i < Secret.Length; i++)
+            {
+                if (Secret[i] == guessNumber[i])
+                {
+                    matched[i] = true; 
+                }
+            }
+
+            for (int i = 0; i < Secret.Length; i++)
+            {
+                for (int j = 0; j < Secret.Length; j++)
+                {
+                    if (i != j && !matched[j] && Secret[j] == guessNumber[i])
+                    {
+                        bCount++;
+                        matched[j] = true; 
+                        break;
+                    }
+                }
+            }
+            return bCount;
+        }
+
+        public bool IsGameOver(string guessNumber)
+        {
+            GuessCount++; // 累計猜測次數
+            Guess = guessNumber;
+            int aCount = numOfA(guessNumber);
+            int bCount = numOfB(guessNumber);
+            Result = $"{aCount}A{bCount}B";
+            GuessHistory.Add($"猜測: {Guess}, 結果: {Result}"); // 紀錄猜測歷史
+
+            return aCount == Secret.Length; // 如果猜對，返回 true
+        }
+
+        public void ResetGame()
+        {
+            Secret = GenerateRandomSecret();
+            GuessHistory.Clear();
+            GuessCount = 0;
+            Guess = null;
+            Result = null;
+        }
+
+        // 所有猜測歷史
+        public string ShowGuessHistory()
+        {
+            return string.Join("\n", GuessHistory);
+        }
+
+        // 累計猜測次數
+        public int ShowGuessCount()
+        {
+            return GuessCount;
+        }
+    }
+}
